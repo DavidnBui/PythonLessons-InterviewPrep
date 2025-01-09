c71e3309699993a67ace8f0e35c7135f5cf244b7
DavidnBui <davidnbui@yahoo.com>
2025-01-09 15:53:17 -0500

    added comments and get method

--- Data_Sctructures/Linked_Lists.py
+++ Data_Sctructures/Linked_Lists.py
@@ -22,8 +22,8 @@ class LinkedList:
             self.head = new_node
             self.tail = new_node
         else:
-            self.tail.next = new_node 
-            self.tail = new_node
+            self.tail.next = new_node  # Adds new node to the end of list
+            self.tail = new_node       # Sets the new note as the tail
         self.length += 1
 
     def pop(self):
@@ -31,11 +31,11 @@ class LinkedList:
             return None
         temp = self.head
         pre = self.head
-        while(temp.next):
-            pre = temp
-            temp = temp.next
-        self.tail = pre
-        self.tail.next = None
+        while(temp.next):              # Iterates through the list as long as temp doesn't hit None
+            pre = temp                 # Pre will now be the second to last node
+            temp = temp.next           # Once temp hits none, It will now point towards the last node 
+        self.tail = pre                # Sets the second to last node as the tail
+        self.tail.next = None          # Removes the last node 
         self.length -= 1
         if self.tail == 0: 
             self.head = None
@@ -43,13 +43,13 @@ class LinkedList:
         return temp
 
     def prepend(self, value):
-        new_node = Node(value) 
+        new_node = Node(value)          
         if self.length == 0:
-            self.head = new_node
-            self.tail = new_node
+            self.head = new_node        
+            self.tail = new_node       
         else:
-            new_node.next = self.head
-            self.head = new_node
+            new_node.next = self.head   # Adds new node to the begining of list 
+            self.head = new_node        # Sets the new node as the head
         self.length += 1
         return True
 
@@ -64,6 +64,13 @@ class LinkedList:
             self.tail = None
         return temp
 
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        for _ in range (index):         # Iterate however many times to reach index
+            temp = temp.next            # Move temp along the list 
+        return temp
 
 my_linked_list = LinkedList(2)
 my_linked_list.append(3)
