7fd71201b37590ef9ecd21be73d8c464f72e7f80
DavidnBui <davidnbui@yahoo.com>
2025-01-12 18:13:19 -0500

    added pop method

--- Data_Sctructures/Doubly_Linked_Lists.py
+++ Data_Sctructures/Doubly_Linked_Lists.py
@@ -28,6 +28,20 @@ class DoublyLinkedList:
             self.tail = new_node            # Set the new node as the tail
         self.length += 1
         return True
+    
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.tail 
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.tail = self.tail.prev 
+            self.tail.next = None
+            temp.prev = None
+        self.length -= 1
+        return temp
 
 my_doubly_linked_list = DoublyLinkedList(7)
 my_doubly_linked_list.print_list()
