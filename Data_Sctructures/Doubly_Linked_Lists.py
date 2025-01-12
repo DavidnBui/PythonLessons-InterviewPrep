9dde05eb8190fa3929645b5f49a5bd2bf5b0f579
DavidnBui <davidnbui@yahoo.com>
2025-01-12 18:31:02 -0500

    added pop_first method

--- Data_Sctructures/Doubly_Linked_Lists.py
+++ Data_Sctructures/Doubly_Linked_Lists.py
@@ -55,6 +55,23 @@ class DoublyLinkedList:
         self.length += 1
         return True
 
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+            self.head.prev = None 
+            temp.next = None 
+        self.length -= 1 
+        return temp
 
-my_doubly_linked_list = DoublyLinkedList(7)
-my_doubly_linked_list.print_list()
+my_doubly_linked_list = DoublyLinkedList(2)
+my_doubly_linked_list.append(1)
+
+print(my_doubly_linked_list.print_list())
+print(my_doubly_linked_list.print_list())
+print(my_doubly_linked_list.print_list())
