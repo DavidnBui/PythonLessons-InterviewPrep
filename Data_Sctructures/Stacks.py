a3541e283144112ab523a111e142ce5d828d5c8a
DavidnBui <davidnbui@yahoo.com>
2025-01-19 17:56:29 -0500

    added pop method

--- Data_Sctructures/Stacks.py
+++ Data_Sctructures/Stacks.py
@@ -24,7 +24,20 @@ class Stack:
             self.top = new_node 
         self.height += 1
 
-my_stack = Stack(2)
-my_stack.push(1)
+    def pop(self):               # removed item at top 
+        if self.height == 0:
+            return None
+        temp = self.top
+        self.top = self.top.next
+        temp.next = None 
+        self.height -= 1
+        return temp
+
+my_stack = Stack(7)
+my_stack.push(23)
+my_stack.push(3)
+my_stack.push(11)
+
+print(my_stack.pop(), '\n')
 
 my_stack.print_stack()
