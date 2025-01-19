9968c2a9898bbc8b2adb6cd407bae55a0d529c19
DavidnBui <davidnbui@yahoo.com>
2025-01-19 17:52:14 -0500

    added push method

--- Data_Sctructures/Stacks.py
+++ Data_Sctructures/Stacks.py
@@ -14,7 +14,17 @@ class Stack:
         while temp is not None:
             print(temp.value)
             temp = temp.next
+    
+    def push(self,value):                   # adds item to top of stack
+        new_node = Node(value)          
+        if self.height == 0:
+            self.top = new_node              
+        else:
+            new_node.next = self.top
+            self.top = new_node 
+        self.height += 1
 
-my_stack = Stack(4)
+my_stack = Stack(2)
+my_stack.push(1)
 
 my_stack.print_stack()
