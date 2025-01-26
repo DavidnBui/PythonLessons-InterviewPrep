b486bc74d0d79cfadb433f1de5071fcbcf08980f
DavidnBui <davidnbui@yahoo.com>
2025-01-25 21:53:12 -0500

    added contain method

--- Data_Sctructures/Trees.py
+++ Data_Sctructures/Trees.py
@@ -8,7 +8,7 @@ class BinarySearchTree:
     def __init__(self):  
         self.root = None  
 
-    def insert(self,value):
+    def insert(self, value):
         new_node = Node(value)
         if self.root is None:
             self.root = new_node
@@ -28,11 +28,27 @@ class BinarySearchTree:
                     return True 
                 temp = temp.right
 
-my_tree = BinarySearchTree()
-my_tree.insert(2)
-my_tree.insert(1)
-my_tree.insert(3)
+    def contains(self, value):
+        if self.root is None:
+            return False
+        temp = self.root
+        while temp is not None:
+            if value < temp.value:
+                temp = temp.left
+            elif value > temp.value:
+                temp = temp.right
+            else:
+                return True
+        return False
 
-print(my_tree.root.value)
-print(my_tree.root.left.value)
-print(my_tree.root.right.value)
+my_tree = BinarySearchTree()
+my_tree.insert(47)
+my_tree.insert(21)
+my_tree.insert(76)
+my_tree.insert(18)
+my_tree.insert(27)
+my_tree.insert(52)
+my_tree.insert(82)
+
+print(my_tree.contains(27))
+print(my_tree.contains(17))
