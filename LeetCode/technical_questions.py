469c4907faa2777c2b68eea42487c54971c221ac
DavidnBui <davidnbui@yahoo.com>
2025-01-26 21:59:18 -0500

    added a technical question

--- LeetCode/technical_questions.py
+++ LeetCode/technical_questions.py
@@ -38,3 +38,16 @@
             if slow == fast:
                 return True
         return False
+
+# When given two lists, 
+# determine if the two lists have a common variable
+    def item_in_common(list1,list2):
+        my_dict = {}
+        for i in list1:
+            my_dict[i] = True
+        
+        for j in list2:
+            if j in my_dict:
+                return True
+            
+        return False
