2b39c106b0c17fe7271885fc205844649f4e1b4a
DavidnBui <davidnbui@yahoo.com>
2025-01-27 19:11:49 -0500

    added sink down method for heaps

--- Data_Sctructures/Heaps.py
+++ Data_Sctructures/Heaps.py
@@ -34,19 +34,42 @@ class MaxHeap:
         self._sink_down(0)
 
         return max_value
+    
+    def _sink_down(self, index):
+        max_index = index 
+        while True:
+            left_index = self._left_child(index)
+            right_index = self._right_child(index)
+
+            if (left_index < len(self.heap) and
+                self.heap[left_index] > self.heap[max_index]):
+                max_index = left_index
+
+            if (right_index < len(self.heap) and 
+                self.heap[right_index] > self.heap[max_index]):
+                max_index = right_index
+
+            if max_index != index:
+                self._swap(index, max_index)
+                index = max_index
+            else:
+                return
 
 
 
 my_heap = MaxHeap()
-my_heap.insert(99)
-my_heap.insert(72)
-my_heap.insert(61)
-my_heap.insert(58)
-
-print(my_heap.heap)
-
-my_heap.insert(100)
-print(my_heap.heap)
-
+my_heap.insert(95)
 my_heap.insert(75)
+my_heap.insert(80)
+my_heap.insert(55)
+my_heap.insert(60)
+my_heap.insert(50)
+my_heap.insert(65)
+
+print(my_heap.heap)
+
+my_heap.remove()
+print(my_heap.heap)
+
+my_heap.remove()
 print(my_heap.heap)
