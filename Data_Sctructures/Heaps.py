a5ff476d6bf19441209ce6b8f26d9cb80f60ab4e
DavidnBui <davidnbui@yahoo.com>
2025-01-27 19:01:16 -0500

    added remove method for heaps

--- Data_Sctructures/Heaps.py
+++ Data_Sctructures/Heaps.py
@@ -22,6 +22,21 @@ class MaxHeap:
             self._swap(current, self._parent(current))
             current = self._parent(current)
 
+    def remove(self):
+        if len(self.heap) == 0:
+            return None
+        
+        if len(self.heap) == 1:
+            return self.heap.pop()
+        
+        max_value = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._sink_down(0)
+
+        return max_value
+
+
+
 my_heap = MaxHeap()
 my_heap.insert(99)
 my_heap.insert(72)
