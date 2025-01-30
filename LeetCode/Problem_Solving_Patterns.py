c4ad524f948607a1f5120c6d0e971d13de91bedd
DavidnBui <davidnbui@yahoo.com>
2025-01-29 23:19:28 -0500

    added two sum problem to leetcode file

--- LeetCode/Problem_Solving_Patterns.py
+++ LeetCode/Problem_Solving_Patterns.py
@@ -1,3 +1,14 @@
 # Problem Solving Patterns 
 
 #Frequency Counter
+
+# Two - Sum 
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dictionary = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in dictionary:
+                return [dictionary[complement], i]
+        dictionary[num] = i
