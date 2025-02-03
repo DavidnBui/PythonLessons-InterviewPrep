5b4f9198c335663a2c05a5865ac3bac72a1ddf87
DavidnBui <davidnbui@yahoo.com>
2025-02-02 19:19:34 -0500

    added a problem solving pattern

--- LeetCode/Problem_Solving_Patterns.py
+++ LeetCode/Problem_Solving_Patterns.py
@@ -3,7 +3,6 @@
 #Frequency Counter
 
 # Two - Sum 
-
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         dictionary = {}
@@ -14,3 +13,19 @@ class Solution:
         dictionary[num] = i
 
 # Two Pointers
+
+# Remove Element
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            if nums[left] == val:
+                nums[left], nums[right] = nums[right], nums[left]
+                right -= 1
+            else: 
+                nums[left] != val
+                left += 1
+        return left
+
