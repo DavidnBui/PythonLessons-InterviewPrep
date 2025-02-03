a76b7f9af0b738a0315935328dc52557951b43ee
DavidnBui <davidnbui@yahoo.com>
2025-02-02 19:22:16 -0500

    added a leetcode problem

--- LeetCode/technical_questions.py
+++ LeetCode/technical_questions.py
@@ -51,3 +51,18 @@
                 return True
         
         return False
+
+# Remove Element
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            if nums[left] == val:
+                nums[left], nums[right] = nums[right], nums[left]
+                right -= 1
+            else: 
+                nums[left] != val
+                left += 1
+        return left
