89d9278e9e14f8d4eb47cb9f4ac17b119759df9e
DavidnBui <davidnbui@yahoo.com>
2025-03-02 17:54:20 -0500

    Saved local changes before pulling

--- LeetCode/technical_questions.py
+++ LeetCode/technical_questions.py
@@ -264,3 +264,28 @@ def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) ->
         tail.next = list1 if list1 else list2           # If any elements remain in list1 or list2, append them
 
         return dummy.next                               # Return merged list (skip dummy node)
+
+# First and Last position of element in sorted array 
+def binary_search(nums, target, is_searching_left):
+            left = 0
+            right = len(nums) - 1
+            idx = -1
+            
+            while left <= right:
+                mid = (left + right) // 2
+                
+                if nums[mid] > target:
+                    right = mid - 1
+                elif nums[mid] < target:
+                    left = mid + 1
+                else:
+                    idx = mid
+                    if is_searching_left:
+                        right = mid - 1
+                    else:
+                        left = mid + 1
+            
+            return idx
+        
+        left = binary_search(nums, target, True)
+        right = binary_search(nums, target, False)
