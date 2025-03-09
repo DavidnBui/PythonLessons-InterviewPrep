582ebfa09c7a0d3a84ffe0b01271b01d15fbe459
DavidnBui <davidnbui@yahoo.com>
2025-03-08 21:37:33 -0500

    fizzbuzz

--- LeetCode/technical_questions.py
+++ LeetCode/technical_questions.py
@@ -382,3 +382,17 @@ def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
             nums1[p] = nums2[p2]
             p2 -= 1
             p -= 1
+
+# Fizzbuzz 
+def fizzBuzz(self, n: int) -> List[str]:
+        result = []
+        for i in range(1, n + 1):
+            if i % 15 == 0:
+                result.append("FizzBuzz")
+            elif i % 3 == 0:
+                result.append("Fizz")
+            elif i % 5 == 0:
+                result.append("Buzz")
+            else:
+                result.append(str(i))
+        return result
